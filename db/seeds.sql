c2f9a7a8620c0d97c1acf6f2c6eb306e51a80544
Claire Cheek <claireccheek@gmail.com>
2021-01-25 18:55:22 -0800

    got everything transfered and working

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Classic Ham');
+INSERT INTO burgers (name) VALUES ('Classic Cheese');
+INSERT INTO burgers (name, eaten) VALUES ('Royale with Cheese', true);
+INSERT INTO burgers (name, eaten) VALUES ('Double Bacon Baby', true);
+INSERT INTO burgers (name, eaten) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name) VALUES ('Fried Chicken Sandwich');
